1334776826af322783b67350d6ec2088177fcac5
Emanuel Paul <emanuel.paul@hotmail.com>
2019-05-30 20:24:36 +0300

    add console project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DI_Configuration_Logging_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
